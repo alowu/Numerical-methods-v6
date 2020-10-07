ea5982849e4ac1cb41346f2829f2a35260f465a2
alo_wu <ilya.bela@yandex.ru>
2020-10-07 23:25:04 +0300

    DONE: lab 4

--- Lab_4/main.cpp
+++ Lab_4/main.cpp
@@ -1,87 +1,100 @@
-#include "../functions/common.h"
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
 //степень аппроксимирующего полинома; число измерений
 
 //const int m = 3;  const int N = 21;  //var 7
-const int m = 2;
-const int N = 10; //var 8
+//const int m = 2; const int N = 10; //var 8
 //const int m = 1; const int N = 5; //var 10
+const int m = 1 , N = 7;
 
 //выделение памяти с возращением указателя
-double **createMatrix(int x) {
-    double **A = new double *[x];
+double** createMatrix(int x)
+{
+    double **A = new double*[x];
     for (int i = 0; i < x; i++)
         A[i] = new double[x + 1];
     return A;
 }
-
 //освобождение памяти
-void deleteMatrix(double **X, int x) {
+void deleteMatrix(double **X, int x)
+{
     for (int i = 0; i < x; i++)
         delete X[i];
     delete[] X;
 }
-
 //заполнение матрицы рандомом
-void enterMatrix(double *X, int x, ifstream &in) {
-    for (int i = 0; i < x; i++) {
-        double element;
-        in >> element;
+void enterMatrix(double *X, int x, ifstream &in)
+{
+    for (int i = 0; i < x; i++)
+    {
+        double element; in >> element;
         X[i] = element;
     }
 }
-
 //отображение в консоли
-void viewMatrix(double **X, int x) {
-    for (int i = 0; i < x; i++) {
+void viewMatrix(double **X, int x)
+{
+    for (int i = 0; i < x; i++)
+    {
         for (int j = 0; j < x + 1; j++)
             cout << setw(15) << X[i][j];
         cout << endl;
     }
     cout << endl << endl;
 }
-
 //копирования для сохранения оригинала
-void copyMatrix(double **X, double **copyX, int x) {
-    for (int i = 0; i < x; i++) {
+void copyMatrix(double **X, double **copyX, int x)
+{
+    for (int i = 0; i < x; i++)
+    {
         for (int j = 0; j < x + 1; j++)
             copyX[i][j] = X[i][j];
     }
 }
-
 //зануляем матрицу
-void zeroMatrix(double **X, int x) {
-    for (int i = 0; i < x; i++) {
-        for (int j = 0; j < x + 1; j++) {
+void zeroMatrix(double **X, int x)
+{
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < x + 1; j++)
+        {
             X[i][j] = 0.0;
         }
     }
 }
-
 //отображение в консоли ответов
-void viewAnswer(double *X, int x) {
+void viewAnswer(double *X, int x)
+{
     for (int i = 0; i < x; i++)
         cout << "a[" << i << "] = " << X[i] << endl;
 }
-
 //метод Гаусса и обратный ход
-bool Gauss(double *An, double **X, int x) {
+bool Gauss(double *An, double **X, int x)
+{
 
-    for (int k = 0; k < x; k++) {
+    for (int k = 0; k < x; k++)
+    {
         double max = fabs(X[k][k]);
-        int remeber = k;        //запоминаем строку, чтобы не поменять саму себя
-        for (int i = k + 1; i < x; i++) {
-            if (max < fabs(X[i][k]))        //находим максимальный по модулю элемент в столбце
+        int remeber = k;		//запоминаем строку, чтобы не поменять саму себя
+        for (int i = k + 1; i < x; i++)
+        {
+            if (max < fabs(X[i][k]))		//находим максимальный по модулю элемент в столбце
             {
                 max = fabs(X[i][k]);
                 remeber = i;
             }
         }
 
-        if (fabs(max - 0) < 1e-6) {
+        if (fabs(max - 0) < 1e-6)
+        {
             return 0;
         }
 
-        if (k != remeber)                //меняем строки местами
+        if (k != remeber)				//меняем строки местами
         {
             double *temp = X[k];
             X[k] = X[remeber];
@@ -90,24 +103,29 @@ bool Gauss(double *An, double **X, int x) {
 
         //viewMatrix(X, row, column);
 
-        double lead = X[k][k];            //запоминаем ведущий элемент
-        for (int r = k; r < x + 1; r++) {
+        double lead = X[k][k];			//запоминаем ведущий элемент
+        for (int r = k; r < x + 1; r++)
+        {
             X[k][r] /= lead;
         }
         //начиная со следующей строки выполняем преобразование Гаусса
-        for (int i = k + 1; i < x; i++) {
+        for (int i = k + 1; i < x; i++)
+        {
             double temp = X[i][k];
-            for (int j = k; j < x + 1; j++) {
+            for (int j = k; j < x + 1; j++)
+            {
                 X[i][j] -= X[k][j] * temp;
             }
         }
         //viewMatrix(A, n, m);
     }
 
-    An[x - 1] = X[x - 1][x + 1 - 1];                //обратный ход
-    for (int i = x - 2; i >= 0; i--) {
+    An[x - 1] = X[x - 1][x + 1 - 1];				//обратный ход
+    for (int i = x - 2; i >= 0; i--)
+    {
         An[i] = X[i][x + 1 - 1];
-        for (int j = i + 1; j < x + 1 - 1; j++) {
+        for (int j = i + 1; j < x + 1 - 1; j++)
+        {
             An[i] -= X[i][j] * An[j];
         }
     }
@@ -115,22 +133,24 @@ bool Gauss(double *An, double **X, int x) {
 }
 
 //среднеквадратическое отклонение
-double valueSigma(double *Datax, double *Datay, double *An, double x) {
+double valueSigma(double *Datax, double *Datay, double *An, double x)
+{
     double sigma = 0, temp;
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < N; i++)	{
         temp = Datay[i];
         for (int j = 0; j < x; j++)
             temp -= An[j] * pow(Datax[i], j);  // вычесляем 4.5
-        sigma += temp * temp;// и в квадрат
+        sigma += temp*temp;// и в квадрат
     }
     return sigma /= (N - m - 1);
 }
 
-int main() {
+int main()
+{
     double *Datax = new double[N];
     double *Datay = new double[N];
-    ifstream inx("file8x.txt");
-    ifstream iny("file8y.txt");
+    ifstream inx(R"(H:\CLionLabs\Numerical-methods-v6\Lab_4\data_x.txt)");
+    ifstream iny(R"(H:\CLionLabs\Numerical-methods-v6\Lab_4\data_y.txt)");
     enterMatrix(Datax, N, inx);
     enterMatrix(Datay, N, iny);
 
@@ -142,9 +162,10 @@ int main() {
 
     for (int i = 0; i < m + 1; i++)   ///  составляем матрицу
     {
-        for (int j = 0; j < m + 1; j++) {
+        for (int j = 0; j < m + 1; j++)
+        {
             for (int k = 0; k < N; k++)
-                A[i][j] += pow(Datax[k], i + j);
+                A[i][j] += pow(Datax[k], i + j);;
         }
     }
     A[0][0] = N;
@@ -167,10 +188,9 @@ int main() {
 
 
 
-    delete[] An;
-    delete[] Datax;
-    delete[] Datay;
+    delete An;
+    delete Datax;
+    delete Datay;
     deleteMatrix(A, m + 1);
     deleteMatrix(copyA, m + 1);
-    system("pause");
 }
